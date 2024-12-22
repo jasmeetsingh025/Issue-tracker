c92cf245c0c4e7c709df36193121acfce4cf4ea2
coding With Jasmeet <jasmeet8022@gmail.com>
2024-12-22 20:16:57 +0530

    Commit

--- config/mongooseDB.js
+++ config/mongooseDB.js
@@ -3,7 +3,12 @@ import mongoose from "mongoose";
 export const connectDb = async () => {
   try {
     console.log("connecting to database...");
-    const res = await mongoose.connect(process.env.mongoURL);
+    const res = await mongoose.connect(
+      `${process.env.mongoURL}/${process.env.DB_NAME}`,
+      {
+        dbName: process.env.DB_NAME,
+      }
+    );
     console.log(`mongodb connected with server ${res.connection.host}`);
   } catch (err) {
     console.log(`mongodb connection failed: ${err.message}`);
